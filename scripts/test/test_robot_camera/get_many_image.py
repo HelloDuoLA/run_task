a8c6838a207cb2805707e69d09cb059c59d5ba4d
elephant01 <elephant01@example.com>
2024-07-17 13:18:17 +0800

    拍摄图片

--- scripts/test/test_robot_camera/get_many_image.py
+++ scripts/test/test_robot_camera/get_many_image.py
@@ -14,10 +14,18 @@ cap2 = cv2.VideoCapture(camera_index2)
 if not cap1.isOpened() or not cap2.isOpened():
     print("无法打开摄像头")
     exit()
+    
+# 设置摄像头分辨率为1280x960
+cap1.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap1.set(cv2.CAP_PROP_FRAME_HEIGHT, 960)
+cap2.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap2.set(cv2.CAP_PROP_FRAME_HEIGHT, 960)
+
+
 
 # 创建保存图像的文件夹
-folder1 = "/home/zrt/xzc_code/Competition/AIRobot/ros_ws/src/run_task/data"
-folder2 = "/home/zrt/xzc_code/Competition/AIRobot/ros_ws/src/run_task/data"
+folder1 = "/home/elephant/xzc_code/ros_ws/run_task/data"
+folder2 = "/home/elephant/xzc_code/ros_ws/run_task/data"
 os.makedirs(folder1, exist_ok=True)
 os.makedirs(folder2, exist_ok=True)
 
@@ -31,17 +39,22 @@ try:
         # 从第二个摄像头读取图像
         ret2, frame2 = cap2.read()
         
+        key = cv2.waitKey(1) & 0xFF
         if ret1 and ret2:
+            # 等待按键事件，参数是等待时间（毫秒）
+            # 如果设置为0，则无限等待直到按键
+            userInput = input("输入'save'保存图像，其他任意键继续: ")
+            # 获取当前时间戳
+            timestamp = int(time.time() * 1000)
             # 构建文件名
-            filename1 = os.path.join(folder1, f"{timestamp}.jpg")
-            filename2 = os.path.join(folder2, f"{timestamp}.jpg")
+            filename1 = os.path.join(folder1, f"{timestamp}_1.jpg")
+            filename2 = os.path.join(folder2, f"{timestamp}_2.jpg")
+            print(f"image1 name {filename1}")
+            print(f"image2 name {filename2}")
             
             # 保存图像
             cv2.imwrite(filename1, frame1)
             cv2.imwrite(filename2, frame2)
-            
-            # 等待1秒
-            time.sleep(1)
         else:
             print("无法从摄像头获取图像")
             break
