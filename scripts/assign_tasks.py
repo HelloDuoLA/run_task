b93d6d00c26ced79fbc66603cabacfdd3a0829ac
Kaydam <1121743758@qq.com>
2024-07-21 16:08:34 +0000

    修改左臂夹下零食动作

--- scripts/assign_tasks.py
+++ scripts/assign_tasks.py
@@ -574,12 +574,15 @@ class Image_rec_actuator():
                     task_grasp_snack:task.Task_manipulation         = current_task.need_modify_tasks.task_list[i*6+1]
                     task_grasp_snack.modify_xyz_select_arm(snack_xyz,arm_id)
                     task_lossen_snack_pre:task.Task_manipulation    = current_task.need_modify_tasks.task_list[i*6+2]
-                    # 左臂松零食使用 X_Y_Z
-                    # 左臂夹下层零食用 XY_Z
+
                     if arm_id == utilis.Device_id.LEFT:
-                        task_lossen_snack_pre.select_arm(arm_id,arm.ArmMoveMethod.X_Z_Y)
+                        # 左臂松零食准备动作使用 Z_X_Y
+                        # 左臂夹下层零食用 XY_Z
                         if snack_xyz[2] < 500:
+                            task_lossen_snack_pre.select_arm(arm_id,arm.ArmMoveMethod.Z_X_Y)
                             task_grasp_snack.arm_move_method = arm.ArmMoveMethod.XY_Z
+                        else:
+                            task_lossen_snack_pre.select_arm(arm_id)
                     else:
                         task_lossen_snack_pre.select_arm(arm_id)
                     task_lossen_snack:task.Task_manipulation        = current_task.need_modify_tasks.task_list[i*6+3]
