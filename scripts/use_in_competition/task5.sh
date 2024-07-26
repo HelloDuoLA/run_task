ea8cff607679ff15e542709559c7110e14d39e4f
elephant01 <elephant01@example.com>
2024-07-26 09:38:27 +0800

    小修改

--- scripts/use_in_competition/task5.sh
+++ scripts/use_in_competition/task5.sh
@@ -3,6 +3,7 @@ source ./devel/setup.bash
 
 # 启动建图节点
 roslaunch run_task mercury_mapping.launch
+# Rviz里可能没有订阅地图
 
 # 启动键盘控制节点
 roslaunch run_task mercury_keyboard_teleop.launch
@@ -10,6 +11,11 @@ roslaunch run_task mercury_keyboard_teleop.launch
 # 使用的线速度和角速度为
 # TODO:待测
 
+# 角速度
+- 0.2 # 左右 C按8下
+# 线速度
+- 0.8  # x按6下
+
 # 保存地图
 # TODO:待修改
-rosrun map_server map_saver -f /home/elephant/xzc_code/ros_ws/src/run_task/map/C111
+rosrun map_server map_saver -f /home/elephant/mercury_x1_ros/src/turn_on_mercury_robot/map/C111xxx
