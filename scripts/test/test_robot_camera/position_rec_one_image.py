7a17dd9756b5bf3d1546e627dcd746f38440fbaa
elephant01 <elephant01@example.com>
2024-08-14 21:57:42 +0800

    小修改

--- scripts/test/test_robot_camera/position_rec_one_image.py
+++ scripts/test/test_robot_camera/position_rec_one_image.py
@@ -156,27 +156,28 @@ if __name__ == "__main__":
         print("left")
         print(f"left   arm end pose {base_coords_left[:3]}")
         # print(f"arm_base_coords {arm_base_coords_left} ")
-        print(f"shousuan  x,y,z:{base_coords_left[0] + xyz[2] - 100} ,{base_coords_left[1] - xyz[1]-78 +20}, {base_coords_left[2] + xyz[0] +45}")
-        print(f"shousuan    xyz:{base_coords_left[0] + xyz[2] - 100}  {base_coords_left[1] - xyz[1]-78 +20} {base_coords_left[2] + xyz[0] +45}")
+        print(f"shousuan  x,y,z:{base_coords_left[0] + xyz[2] - 100} ,{base_coords_left[1] - xyz[1]-78 + 10}, {base_coords_left[2] + xyz[0] + 20}")
+        print(f"shousuan    xyz:{base_coords_left[0] + xyz[2] - 100}  {base_coords_left[1] - xyz[1]-78 + 10} {base_coords_left[2] + xyz[0]  + 20}")
         print("right")
         print(f"right  arm end pose {base_coords_right[:3]}")
         # print(f"arm_base_coords {arm_base_coords_right} ")
-        print(f"shousuan  x,y,z:{base_coords_right[0] + xyz[2] - 90 } , {base_coords_right[1] + xyz[1] + 78 -20}, {base_coords_right[2] - xyz[0] +20}\r\n\r\n")
-        print(f"shousuan    xyz:{base_coords_right[0] + xyz[2] - 90 }  {base_coords_right[1] + xyz[1] + 78 -20} {base_coords_right[2] - xyz[0] +20}\r\n\r\n")
+        print(f"shousuan  x,y,z:{base_coords_right[0] + xyz[2] - 80 } , {base_coords_right[1] + xyz[1] + 78 -20}, {base_coords_right[2] - xyz[0] + 20}\r\n\r\n")
+        print(f"shousuan    xyz:{base_coords_right[0] + xyz[2] - 80 }  {base_coords_right[1] + xyz[1] + 78 -20} {base_coords_right[2] - xyz[0]   + 20}\r\n\r\n")
+
     
 # 左臂
 # 测量 404.9347297360397  159.7166483771058 380.6525213499157
 # 实际 394, 180, 380
 
-# 上排550
-# 下排380
+# 上排 465
+# 下排 260
 
 
 # 右臂
 # 测量 394, 180, 380
 # 实际 
-# 上排550
-# 下排400
+# 上排450
+# 下排280
 
 
 
